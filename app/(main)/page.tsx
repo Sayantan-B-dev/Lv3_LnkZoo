e20eea92c428ec192fd82ffa8194860e412faff3
Sayantan <sayantanbharati611@gmail.com>
2026-06-04 18:22:22 +0530

    home: move components into app/(main), fix footer hover color, make cursor always invert via pure #fff + mix-blend-mode: difference

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,16 +3,16 @@
 import React, { useState, useEffect } from 'react';
 import Topbar from '@/components/common/Topbar';
 import { useRouter } from 'next/navigation';
-import { useScrollProgress } from '@/home-components/hooks';
-import { HeroSection } from '@/home-components/HeroSection';
-import { MarqueeSection } from '@/home-components/MarqueeSection';
-import { AboutSection } from '@/home-components/AboutSection';
-import { FeaturesSection } from '@/home-components/FeaturesSection';
-import { MetricsSection } from '@/home-components/MetricsSection';
-import { FeedSection } from '@/home-components/FeedSection';
-import { FAQSection } from '@/home-components/FAQSection';
-import { CTASection } from '@/home-components/CTASection';
-import { FooterSection } from '@/home-components/FooterSection';
+import { useScrollProgress } from './home-components/hooks';
+import { HeroSection } from './home-components/HeroSection';
+import { MarqueeSection } from './home-components/MarqueeSection';
+import { AboutSection } from './home-components/AboutSection';
+import { FeaturesSection } from './home-components/FeaturesSection';
+import { MetricsSection } from './home-components/MetricsSection';
+import { FeedSection } from './home-components/FeedSection';
+import { FAQSection } from './home-components/FAQSection';
+import { CTASection } from './home-components/CTASection';
+import { FooterSection } from './home-components/FooterSection';
 
 export default function Home() {
   const router = useRouter();
